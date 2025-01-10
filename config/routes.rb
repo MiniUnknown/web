296ced1487c5943d21dffa62b5094f706ab5c68a
MiniUnknown <miniunknown2k@gmail.com>
2025-01-10 13:24:03 +0700

    update2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+
   root "products#index"
 
 end
